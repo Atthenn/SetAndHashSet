35406ddace94bd4a85b46fb3c0c43b1798a97361
atena kalaki <atena.kalaki@gmail.com>
2020-05-28 09:15:35 +1000

    Initial commit

---
+++ src/com/company/Dog.java
@@ -0,0 +1,28 @@
+package com.company;
+
+import java.util.Objects;
+
+public class Dog {
+    private final String name;
+
+    public Dog(String name){
+        this.name = name;
+    }
+
+    public String getName(){
+        return name;
+    }
+
+    @Override
+    public final boolean equals(Object o) {
+        if (this == o) return true;
+        if(o instanceof Dog){
+            String objName = ((Dog) o).getName();
+            return this.name.equals(objName);
+        }
+
+        return false;
+    }
+
+
+}
